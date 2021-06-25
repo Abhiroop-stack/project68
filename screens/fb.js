26c57bd5c025b16dbe951c0c5f621ff648f7269f
Abhiroop-stack <72975864+Abhiroop-stack@users.noreply.github.com>
2021-06-25 12:48:00 +0530

    Add files via upload

---
+++ screens/fb.js
@@ -0,0 +1,19 @@
+
+import * as React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import fbScreen from './Screens/fbScreen'
+import instaScreen from './Screens/instaScreen'
+
+export default class Searchscreen extends React.Component {
+    render() {
+      return (
+        <View>
+        <View>  <AppHeader/> </View>
+        <View style={styles.insta}>
+          <Text style={styles.instaStyles}>INSTA</Text>
+        </View></View>
+      );
+    }
+  }
